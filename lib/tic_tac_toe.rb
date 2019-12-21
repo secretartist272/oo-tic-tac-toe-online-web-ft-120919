517ed7ea29544e011fcd266b9c7de17709177996
Jacqueline Freeland <secretartist272@gmail.com>
2019-12-21 12:58:31 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -52,7 +52,8 @@ class TicTacToe
   end
   
   def current_player
-  
+  #if the turn count is an even number, that means O just went, so the next/current player is X
+  num_turns = turn_count
   if num_turns % 2 == 0
     player = "X"
   else
