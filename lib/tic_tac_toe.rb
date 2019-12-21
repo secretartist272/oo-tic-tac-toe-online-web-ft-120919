dc6e24324973c5c4311b729efb29366b4e2a3014
Jacqueline Freeland <secretartist272@gmail.com>
2019-12-21 13:00:08 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -70,20 +70,20 @@ class TicTacToe
       player_token = current_player
       move(index, player_token)
       display_board
-  else
-    turn
-  end
+    else
+      turn
+    end
   end
   
   def won?
-  WIN_COMBINATIONS.each {|win_combo|
-    index_0 = win_combo[0]
-    index_1 = win_combo[1]
-    index_2 = win_combo[2]
-  
-    position_1 = @board[index_0]
-    position_2 = @board[index_1]
-    position_3 = @board[index_2]
+    WIN_COMBINATIONS.each {|win_combo|
+      index_0 = win_combo[0]
+      index_1 = win_combo[1]
+      index_2 = win_combo[2]
+    
+      position_1 = @board[index_0]
+      position_2 = @board[index_1]
+      position_3 = @board[index_2]
   
     if position_1 == "X" && position_2 == "X" && position_3 == "X"
       return win_combo
