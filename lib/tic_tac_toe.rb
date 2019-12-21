2870557f6567c7f2587c1d5eec67c9ff93c0668d
Jacqueline Freeland <secretartist272@gmail.com>
2019-12-21 13:00:23 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -85,13 +85,13 @@ class TicTacToe
       position_2 = @board[index_1]
       position_3 = @board[index_2]
   
-    if position_1 == "X" && position_2 == "X" && position_3 == "X"
-      return win_combo
-    elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
-      return win_combo
-    end
-  }
-  return false
+      if position_1 == "X" && position_2 == "X" && position_3 == "X"
+        return win_combo
+      elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
+        return win_combo
+      end
+    }
+    return false
   end
   
   def full?
