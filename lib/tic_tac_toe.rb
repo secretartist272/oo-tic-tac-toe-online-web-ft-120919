ebaad982b0556c7e8fc82fd026d129b6845d00d6
Jacqueline Freeland <secretartist272@gmail.com>
2019-12-21 13:00:56 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -115,17 +115,17 @@ class TicTacToe
   end
   
   def winner
-  index = []
-  index = won?
-  if index == false
-    return nil
-  else
-    if @board[index[0]] == "X"
-      return "X"
+    index = []
+    index = won?
+    if index == false
+      return nil
     else
-      return "O"
+      if @board[index[0]] == "X"
+        return "X"
+      else
+        return "O"
+      end
     end
-  end
   end
   
   def play
